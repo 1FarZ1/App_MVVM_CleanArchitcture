1376a1b50743db7030cab725a66790c60f24bf7b
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2022-10-31 20:01:05 +0100

    adding caching

--- lib/data/DataSource/local_datasource.dart
+++ lib/data/DataSource/local_datasource.dart
@@ -1,9 +1,11 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first, non_constant_identifier_names
+// ignore_for_file: public_member_api_docs, sort_constructors_first, non_constant_identifier_names, constant_identifier_names
 import 'package:providerlearn/data/Network/app_api.dart';
+import 'package:providerlearn/data/Network/error_handler.dart';
 import 'package:providerlearn/data/Network/requests.dart';
 import 'package:providerlearn/data/Response/responses.dart';
 
 const CacheHomeKey = "Cache_Home_Key";
+const CacheHomeInterval = 60 * 1000; // 1 minute Cache in millis
 
 abstract class LocalDataSource {
   Future<HomeResponse> getHome();
@@ -14,13 +16,19 @@ class LocalDataSourceeImpl implements LocalDataSource {
   Map<String, CachedItem> CacheMap = Map();
   @override
   Future<HomeResponse> getHome() async {
-    throw UnimplementedError;
+    CachedItem? cachedItem = CacheMap[CacheHomeKey];
+
+    if (cachedItem != null && !cachedItem.isExpired) {
+      // return response from cache
+      return cachedItem.data;
+    } else {
+      // return an error that cache is not there or its not valid
+    }
   }
-  
+
   @override
   Future<void> SaveHomeToCache(homeresponse) async {
-
-  CacheMap[CacheHomeKey]=CachedItem(homeresponse);
+    CacheMap[CacheHomeKey] = CachedItem(homeresponse);
   }
 }
 
@@ -29,6 +37,11 @@ class CachedItem {
   int cacheTime = DateTime.now().millisecondsSinceEpoch;
 
   CachedItem(this.data);
+}
+
+extension CachedItemExtension on CachedItem {
+  bool get isExpired =>
+      DateTime.now().millisecondsSinceEpoch - cacheTime >= CacheHomeInterval;
 }
 
 //TODO: FIX THE NULL Problem
--- lib/data/DataSource/new.kt
+++ lib/data/DataSource/new.kt
@@ -1,5 +1,32 @@
 
 fun main(){
+    val  x=30;
+    val y=20;
+    val z=10;
+    val result = x+y+z;
+    println(result);
 
-    print("HELLO")
+    // 2nd way
+    val result2 = x.plus(y).plus(z);
+    println(result2);
+
+    // 3rd way
+    val result3 = x.plus(y.plus(z));
+    println(result3);
+
+    // 4th way
+    val result4 = x.plus(y).plus(z);
+    println(result4);
+
+    // 5th way
+    val result5 = x.plus(y).plus(z);
+    println(result5);
+
+
+    val name = "Rahul"
+    val age = 20
+    val result6 = "My name is $name and my age is $age"
+    println(result6)
+    
+
 }
